0bc5a0f797a1a247f8ce10bbf982902be9b0d4e4
José Augusto <jaugustorc@gmail.com>
2024-10-05 16:05:00 -0300

    start

---
+++ DevFreela.Application/Notification/ProjectCreated/GenerateProjectBoardHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace DevFreela.Application.Notification.ProjectCreated
+{
+    public class GenerateProjectBoardHandler : INotificationHandler<ProjectCreatedNotification>
+    {
+        public Task Handle(ProjectCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Criando painel para o projeto {notification.Title}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQuery.cs
@@ -0,0 +1,10 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Queries.GetAllProjects
+{
+    public class GetAllProjectsQuery : IRequest<ResultViewModel<List<ProjectItemViewModel>>>
+    {
+        
+    }
+}
