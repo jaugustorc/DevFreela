309efbd2475ffbca09a705ef33a19a65d2013b3e
jaugustorc <jaugustorc@gmail.com>
2024-10-16 08:12:25 -0300

    normalizado ao que foi visto na aula, falta testar os demais.

---
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using DevFreela.Application.Commands.InsertProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using MediatR;
+using NSubstitute;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            // Arrange
+            var projectRepository = Substitute.For<IProjectRepository>();
+            var mediatoR = Substitute.For<IMediator>();
+
+            var createProjectCommand = new InsertProjectCommand
+            {
+                Title = "Titulo de Teste",
+                Description = "Uma descrição Daora",
+                TotalCost = 50000,
+                IdClient = 1,
+                IdFreelancer = 2
+            };
+
+            var createProjectCommandHandler = new InsertProjectHandler(mediatoR, projectRepository);
+
+            // Act
+            var response = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
+
+            // Assert
+            Assert.True(response.Data >= 0);
+
+            await projectRepository.Received(1).Add(Arg.Any<Project>());
+        }
+    }
+}
---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using NSubstitute;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
+        {
+            // Arrange
+            User client = new("", "", DateTime.Now, "", "");
+            User freela = new("", "", DateTime.Now, "", "");
+            Project project = new("Nome Do Teste 1", "Descricao De Teste 1", 1, 2, 10000);
+
+            typeof(Project)
+                .GetProperty("Client")
+                .SetValue(project, client);
+            typeof(Project)
+                .GetProperty("Freelancer")
+                .SetValue(project, freela);
+
+
+            var projects = new List<Project>
+                { project, project, project };
+
+            var projectRepository = Substitute.For<IProjectRepository>();
+            projectRepository.GetAll().Returns(projects);
+
+            var getAllProjectsQuery = new GetAllProjectsQuery();
+            var getAllProjectsQueryHandler = new GetAllProjectsHandler(projectRepository);
+
+            // Act
+            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList.Data);
+            Assert.Equal(projects.Count, projectViewModelList.Data.Count);
+
+            // Verifica se o método GetAllAsync foi chamado uma vez
+            await projectRepository.Received(1).GetAll();
+        }
+    }
+}
